20150497f1ed40df21e6ebdb9c4d4392f65366f6
Krishnan Sriram <krishnan.sriram@hotmail.com>
2019-09-20 13:20:05 -0400

    First commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Navigator from './components/navigator';
+import Header from './components/header';
+import BodySection from './components/bodysection';
+import Footer from './components/footer';
+
+function App() {
+  return (
+    <div>
+      <Navigator />
+      <Header />
+      <BodySection />
+      <Footer />
+    </div>
+  
+
+  
+
+
+  );
+}
+
+export default App;
